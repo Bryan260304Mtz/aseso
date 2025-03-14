cb1b0c2f602fe8060ad5e7435fb9f1048ec56a1e
Bryan260304Mtz <mtzbryan68@gmail.com>
2025-03-14 14:00:00 -0600

    5to commit

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,7 +225,7 @@
 
     <div class="heroe">
 
-        <h1>anjkahd <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Bienvenido desde la Lap del Bryan <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
